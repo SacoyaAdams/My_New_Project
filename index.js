9a5ec137da03178d1b9f0bf6837c058fe3e5a5f4
Sacoya <sacoyaengineer@gmail.com>
2023-01-16 20:31:17 -0500

    d\fetch request

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
         input.addEventListener('click', function() {
             alert('Lets Explore Makeup Products !!!');
           })
+          console.log(e.target.search.value)
+          fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${e.target.search.value}`)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
