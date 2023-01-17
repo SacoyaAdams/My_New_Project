7757ac2bcdd25fe474d3c6ff077c406bf587ce39
Sacoya <sacoyaengineer@gmail.com>
2023-01-16 20:25:15 -0500

    form submit

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("The DOM has loaded");
+    const input = document.getElementById('new_search');
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault() 
