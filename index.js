ff27b78160f10812fa6c23913caa2db23e754b0e
Sacoya <sacoyaengineer@gmail.com>
2023-01-16 20:26:33 -0500

    create alert

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const input = document.getElementById('new_search');
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault() 
+        input.addEventListener('click', function() {
+            alert('Lets Explore Makeup Products !!!');
+          })
